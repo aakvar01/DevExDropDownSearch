7c7d77df8fe3502929e0606cbeb955b63d559472
Varshney, Aakash <aakvar01@protiviti.com>
2018-08-20 22:28:32 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,15 @@
-import { Component } from '@angular/core';
+import { Component,  Inject } from '@angular/core';
 
+import { Http } from '@angular/http';
+import  'rxjs/operator/toPromise';
+
+import {
+    DxDropDownBoxModule,
+    DxDataGridModule
+} from 'devextreme-angular';
+
+import CustomStore from 'devextreme/data/custom_store';
+import DataSource from 'devextreme/data/data_source';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +17,91 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DevExddSearch';
+
+  gridDataSource: any;
+  _gridBoxValue: number;
+  gridBoxOpened: boolean;
+  searchTimer: number;
+  _gridSelectedRowKeys: number[] = [3];
+
+  constructor(@Inject(Http) http: Http) {
+      this.gridDataSource = new DataSource({
+          searchExpr: ["CompanyName", "City", "Phone"],
+          store: new CustomStore({
+              loadMode: "raw",
+              key: "ID",
+              load: function() {
+                  return http.get('assets/data/customers.json')
+                      .toPromise()
+                      .then(response => {
+                          return response.json();
+                      });
+              }
+          })
+      });
+  }
+
+  isSearchIncomplete(dropDownBox){
+      var displayValue = dropDownBox.option("displayValue"),
+          text = dropDownBox.option("text");
+
+      text = text && text.length && text[0];
+      displayValue = displayValue && displayValue.length && displayValue[0];
+
+      return text !== displayValue;
+  }
+
+  grid_onInput(e){
+      clearTimeout(this.searchTimer);
+      this.searchTimer = setTimeout(function() {
+          var text = e.component.option("text");
+
+          this.gridDataSource.searchValue(text);
+          if(this.isSearchIncomplete(e.component)) {
+              this.gridBoxOpened = true;
+              this.gridDataSource.load();
+          }
+      }.bind(this), 1000);
+  }
+
+  grid_valChange(e){
+
+
+debugger;
+
+  }
+  grid_onClosed(e){
+      var value = this._gridBoxValue,
+          searchValue = this.gridDataSource.searchValue();
+
+      if (this.isSearchIncomplete(e.component)){
+          e.component.reset();
+          e.component.option("value", value);
+      }
+
+      
+      if (searchValue) {
+          this.gridDataSource.searchValue(null);
+          this.gridDataSource.load();
+      }
+  }
+
+  get gridBoxValue(value: number)  {
+      return this._gridBoxValue;
+  }
+
+  set gridBoxValue(value: number) {
+      this._gridSelectedRowKeys = value && [value] || [];
+      this._gridBoxValue = value;
+      this.gridBoxOpened = true;
+  }
+
+  get gridSelectedRowKeys(): number[] {
+      return this._gridSelectedRowKeys;
+  }
+
+  set gridSelectedRowKeys(value: [number]) {
+      this._gridBoxValue = value.length && value[0] || null;
+      this._gridSelectedRowKeys = value;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,16 @@
+import { NgModule, Component, ViewChild, Inject } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { Http, HttpModule } from '@angular/http';
+
+
+import {
+    DxDropDownBoxModule,
+    DxDataGridModule
+} from 'devextreme-angular';
+
+import CustomStore from 'devextreme/data/custom_store';
+import DataSource from 'devextreme/data/data_source';
 
 import { AppComponent } from './app.component';
 
@@ -8,9 +19,13 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DxDropDownBoxModule,
+    HttpModule,
+    DxDataGridModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+platformBrowserDynamic().bootstrapModule(AppModule);
