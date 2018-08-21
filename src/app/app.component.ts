a1a0959185424adb52077dc294ae706889f1e347
Varshney, Aakash <aakvar01@protiviti.com>
2018-08-21 22:49:25 +0530

    Second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,13 @@
 import { Component,  Inject } from '@angular/core';
-
 import { Http } from '@angular/http';
 import  'rxjs/operator/toPromise';
-
 import {
     DxDropDownBoxModule,
     DxDataGridModule
 } from 'devextreme-angular';
+import CustomStore from "devextreme/data/custom_store";
+import DataSource from "devextreme/data/data_source";
 
-import CustomStore from 'devextreme/data/custom_store';
-import DataSource from 'devextreme/data/data_source';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -17,91 +15,116 @@ import DataSource from 'devextreme/data/data_source';
 })
 export class AppComponent {
   title = 'DevExddSearch';
-
   gridDataSource: any;
-  _gridBoxValue: number;
-  gridBoxOpened: boolean;
+  _gridBoxValue: number = null;
+  gridBoxOpened: boolean = false;
   searchTimer: number;
-  _gridSelectedRowKeys: number[] = [3];
+  _gridSelectedRowKeys: number[]=[1];
 
   constructor(@Inject(Http) http: Http) {
-      this.gridDataSource = new DataSource({
-          searchExpr: ["CompanyName", "City", "Phone"],
-          store: new CustomStore({
-              loadMode: "raw",
-              key: "ID",
-              load: function() {
-                  return http.get('assets/data/customers.json')
-                      .toPromise()
-                      .then(response => {
-                          return response.json();
-                      });
-              }
-          })
+
+    this.gridDataSource = this.makeAsyncDataSource(http, "customers.json");
+
+    //   this.gridDataSource = new DataSource({
+    //       searchExpr: ["CompanyName", "City", "Phone"],
+    //       store: new CustomStore({
+    //           loadMode: "raw",
+    //           key: "ID",
+    //           load: function() {
+    //               return http.get('assets/data/customers.json')
+    //                   .toPromise()
+    //                   .then(response => {
+    //                       return response.json();
+    //                   });
+    //           }
+    //       })
+    //   });
+  }
+
+  makeAsyncDataSource(http, jsonFile) {
+    return new DataSource({
+      searchExpr: ["CompanyName", "City", "Phone"],
+      store: new CustomStore({
+        loadMode: "raw",
+        key: "ID",
+        load: function() {
+          return http
+            .get(
+              "https://js.devexpress.com/Demos/WidgetsGallery/JSDemos/data/" +
+                jsonFile
+            )
+            .toPromise() .then(response => {
+                                    return response.json();
+                            });
+        }
+      })
+    });
+  }
+  isSearchIncomplete(dropDownBox) {
+    let displayValue = dropDownBox.option("displayValue"),
+      text = dropDownBox.option("text");
+    text = text && text.length && text[0];
+    displayValue = displayValue && displayValue.length && displayValue[0];
+    return text !== displayValue;
+  }
+ 
+  onFocusOut(e) {
+    var $target = e.event.relatedTarget;
+    if (e.component.content().parentNode === $target) {
+      setTimeout(function() {
+        e.component.focus();
       });
+    }
   }
+  grid_onInput(e) {
+    clearTimeout(this.searchTimer);
+    this.searchTimer = setTimeout(
+      function() {
+        const text = e.component.option("text");
 
-  isSearchIncomplete(dropDownBox){
-      var displayValue = dropDownBox.option("displayValue"),
-          text = dropDownBox.option("text");
-
-      text = text && text.length && text[0];
-      displayValue = displayValue && displayValue.length && displayValue[0];
-
-      return text !== displayValue;
-  }
-
-  grid_onInput(e){
-      clearTimeout(this.searchTimer);
-      this.searchTimer = setTimeout(function() {
-          var text = e.component.option("text");
-
-          this.gridDataSource.searchValue(text);
-          if(this.isSearchIncomplete(e.component)) {
-              this.gridBoxOpened = true;
-              this.gridDataSource.load();
-          }
-      }.bind(this), 1000);
-  }
-
-  grid_valChange(e){
-
-
-debugger;
-
-  }
-  grid_onClosed(e){
-      var value = this._gridBoxValue,
-          searchValue = this.gridDataSource.searchValue();
-
-      if (this.isSearchIncomplete(e.component)){
-          e.component.reset();
-          e.component.option("value", value);
-      }
-
-      
-      if (searchValue) {
-          this.gridDataSource.searchValue(null);
+        this.gridDataSource.searchValue(text);
+        if (this.isSearchIncomplete(e.component)) {
+          this.gridBoxOpened = true;
           this.gridDataSource.load();
-      }
+          
+        }
+      }.bind(this),
+      10
+    );
   }
 
-  get gridBoxValue(value: number)  {
-      return this._gridBoxValue;
+  grid_onClosed(e) {
+    const value = this._gridBoxValue,
+      searchValue = this.gridDataSource.searchValue();
+
+    if (this.isSearchIncomplete(e.component)) {
+      e.component.reset();
+      e.component.option("value", value);
+    }
+
+    if (searchValue) {
+      this.gridDataSource.searchValue(null);
+      this.gridDataSource.load();
+    }
+  }
+
+  get gridBoxValue(): number {
+    return this._gridBoxValue;
   }
 
   set gridBoxValue(value: number) {
-      this._gridSelectedRowKeys = value && [value] || [];
-      this._gridBoxValue = value;
-      this.gridBoxOpened = true;
+    this._gridSelectedRowKeys = (value && [value]) || [];
+    this._gridBoxValue = value;
+
+    setTimeout(() => (this.gridBoxOpened = false));
   }
 
   get gridSelectedRowKeys(): number[] {
-      return this._gridSelectedRowKeys;
+    return this._gridSelectedRowKeys;
   }
 
-  set gridSelectedRowKeys(value: [number]) {
-      this._gridBoxValue = value.length && value[0] || null;
-      this._gridSelectedRowKeys = value;
+  set gridSelectedRowKeys(value: number[]) {
+    this._gridBoxValue = (value.length && value[0]) || null;
+    this._gridSelectedRowKeys = value;
   }
 }
